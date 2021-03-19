9381fc4bbfeb193f2de85e772e23a77e50594e0a
Roy <lxysac@foxmail.com>
2021-03-19 14:17:54 +0800

    中介者模式

---
+++ MediatorPattern/ChatRoom.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatorPattern
+{
+    public class ChatRoom
+    {
+        public static void ShowMsg(User user,string msg)
+        {
+            Console.WriteLine("["+user.Name+"]:"+msg);
+        }
+    }
+}
---
+++ MediatorPattern/User.cs
@@ -0,0 +1,17 @@
+﻿namespace MediatorPattern
+{
+    public class User
+    {
+        public string Name { get; internal set; }
+
+        public User(string name)
+        {
+            Name = name;
+        }
+
+        public void SendMsg(string msg)
+        {
+            ChatRoom.ShowMsg(this,msg);
+        }
+    }
+}
